a093fba72f16b674fb1ca12a22cb4f92e4aaa5b9
Michael <michaeln870@gmail.com>
2020-09-17 14:34:55 -0400

    Removed legend box

--- plot4.R
+++ plot4.R
@@ -19,7 +19,7 @@ plot(datetime, hpc$Global_active_power, type = "l", xlab = "", ylab = "Global Ac
 plot(datetime, hpc$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering",)
 lines(datetime, hpc$Sub_metering_2, type = "l", xlab = "", col = "red",)
 lines(datetime, hpc$Sub_metering_3, type = "l", xlab = "", col = "blue",)
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1),  col=c("black","red","blue"))
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1),  col=c("black","red","blue"),  bty = "n")
 
 #plot 3
 plot(datetime, hpc$Voltage,
